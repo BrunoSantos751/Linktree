719519f935cb973000f75cb6518c43e324598c11
Brunosantos751 <brunomoraesleao@hotmail.com>
2025-09-23 14:03:09 -0300

    rota admin

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -1,14 +1,4 @@
 export async function onRequest(context) {
-  const { env, request } = context;
 
-  // URL de callback
-  const redirectUri = `${new URL(request.url).origin}/api/auth/callback`;
-
-  // Construir URL de login do GitHub
-  const githubLoginUrl = new URL("https://github.com/login/oauth/authorize");
-  githubLoginUrl.searchParams.set("client_id", env.GITHUB_CLIENT_ID);
-  githubLoginUrl.searchParams.set("redirect_uri", redirectUri);
-  githubLoginUrl.searchParams.set("scope", "repo,user:email");
-
-  return Response.redirect(githubLoginUrl.toString(), 302);
+  return Response.redirect("https://linktr33.sanity.studio/", 302);
 }
