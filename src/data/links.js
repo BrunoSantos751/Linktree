10ce8f1526100486103005ec884ffa82fb1a56a6
Brunosantos751 <brunomoraesleao@hotmail.com>
2025-09-23 04:52:56 -0300

    inicio do projeto linktree

---
+++ src/data/links.js
@@ -0,0 +1,19 @@
+export const socialLinks = [
+  {
+    title: 'Meu Portfólio',
+    url: 'https://seusite.com',
+    highlight: true, 
+  },
+  {
+    title: 'LinkedIn',
+    url: 'www.linkedin.com/in/bruno-santos-moraes-943091253',
+  },
+  {
+    title: 'GitHub',
+    url: 'https://github.com/BrunoSantos751',
+  },
+  {
+    title: 'Entre em Contato por E-mail',
+    url: 'mailto:brunomoraesleao@hotmail.com',
+  },
+];
