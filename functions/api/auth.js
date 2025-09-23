a177f9532f7d86f885a21440d8e2c5663d96dd68
Brunosantos751 <brunomoraesleao@hotmail.com>
2025-09-23 11:38:28 -0300

    auth

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -1,5 +1,13 @@
-export async function onRequest(context) {
-  return new Response("Hello from Cloudflare Function!", {
-    headers: { "content-type": "text/plain" },
+import { oauthHandler } from 'decap-cms-oauth-provider-node';
+
+export default function handler(req, res) {
+  return oauthHandler(req, res, {
+    // ID e segredo do GitHub
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+    // URL de callback (a mesma cadastrada no GitHub)
+    redirect_uri: `${process.env.OAUTH_REDIRECT_URL}`,
+    // Domínios permitidos a acessar o CMS
+    allowedOrigins: ['https://linktree-214.pages.dev'],
   });
 }
